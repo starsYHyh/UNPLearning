19a7f402d6a1a709d635a459c66c74f555ecb347
starsYHyh <2976604815@qq.com>
2024-06-24 15:54:30 +0800

    set env

---
+++ intro/build/CMakeFiles/DAYTIMECPCLI.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/DAYTIMECPCLI"
+  "../bin/DAYTIMECPCLI.pdb"
+  "CMakeFiles/DAYTIMECPCLI.dir/home/firefly/Codes/C/UNP/lib/error.c.o"
+  "CMakeFiles/DAYTIMECPCLI.dir/home/firefly/Codes/C/UNP/lib/error.c.o.d"
+  "CMakeFiles/DAYTIMECPCLI.dir/src/daytimetcpcli.c.o"
+  "CMakeFiles/DAYTIMECPCLI.dir/src/daytimetcpcli.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DAYTIMECPCLI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ intro/build/CMakeFiles/DAYTIMECPCLI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DAYTIMECPCLI.
+# This may be replaced when dependencies are built.
