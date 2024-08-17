ee18bd331097177c891ad7f61a3cb5fba5791796
starsYHyh <2976604815@qq.com>
2024-08-18 00:00:57 +0800

    update structure

---
+++ tcpcliserv/build/CMakeFiles/TCPSERV08.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/TCPSERV08"
+  "../bin/TCPSERV08.pdb"
+  "CMakeFiles/TCPSERV08.dir/lib/sigchldwait.c.o"
+  "CMakeFiles/TCPSERV08.dir/lib/sigchldwait.c.o.d"
+  "CMakeFiles/TCPSERV08.dir/src/tcpserv08.c.o"
+  "CMakeFiles/TCPSERV08.dir/src/tcpserv08.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TCPSERV08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
